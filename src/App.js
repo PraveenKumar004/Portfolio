759989a248bc9f0d6844555f2ee94daae76847ed
PraveenKumar004 <praveenkumarv989@gmail.com>
2024-04-03 11:04:59 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
    <>
      <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/Portfolio' element={<Home/>}/>
         </Routes>
       </BrowserRouter>
    </>
