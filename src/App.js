5bb17382e16ebdf156a5fb9f2c165fb13f1c2967
PraveenKumar004 <praveenkumarv989@gmail.com>
2024-05-28 21:22:43 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
-
+import NoPage from './pages/noPage';
+import Sample from './pages/sample';
 
 function App() {
   return (
    <>
      <BrowserRouter>
         <Routes>
-        <Route path='/Portfolio' element={<Home/>}/>
-        <Route path='*' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sample' element={<Sample/>}/>
+        <Route path='*' element={<NoPage/>}/>
         </Routes>
       </BrowserRouter>
    </>
